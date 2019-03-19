3f91fd7916f77628732dc05539edb2d5d9a0e122
kurookamy <48729669+kurookamy@users.noreply.github.com>
2019-03-19 18:21:04 +0100

    tipos de variables

---
+++ PRACTICA01.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ /*
+package practica01;
+
+//**
+ *
+ * @author PC15-LAB02
+ */
+public class PRACTICA01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLE BOOLEAR,DECLACION
+        boolean bool1;
+        //INICIALIZACION
+        bool1=true;
+        //DECLACION Y INICIALIZACION
+        boolean bool2=false;
+        System.out.println("valor bool1:"+bool1);
+        System.out.println("valor bool2:"+bool2);
+      System.out.println("");
+    }
+    
+}
