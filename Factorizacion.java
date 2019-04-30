9c226f49a582bfe3b5fde74d7efa667f11365a5c
kurookamy <48729669+kurookamy@users.noreply.github.com>
2019-04-29 22:29:53 -0500

    Add files via upload

---
+++ Factorizacion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorizacion;
+
+/**
+ *
+ * @author victor
+ */
+public class Factorizacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int m=1;
+        int s=0;
+        int n;
+        for(int i=0;i<=m;i++){
+        if (i==0){
+        print i+"!=1";}
+        else{
+                m=1*m;
+                s=m+s;
+                print i+"!"+"="m;
+    }
+    }
+    s=s+1;
+            print"suma de los"+s;
+}
