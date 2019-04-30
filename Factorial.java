38560918eda3355f6c2cee2c0b9f96d351e8806c
kurookamy <48729669+kurookamy@users.noreply.github.com>
2019-04-29 22:28:21 -0500

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+import java.util.Scanner;
+/**
+ *
+ * @author PC15-LAB02
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int hd;
+        Scanner sc=new Scanner(System.in);
+                hd= sc.nextInt();
+                System.out.println("ingreso:"+hd);
+    }
+}
---
+++ HORAS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horas;
+
+/**
+ *
+ * @author PC15-LAB02
+ */
+public class HORAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        // TODO code application logic here
+        System.out.println("este programa simula la hora en un reloj");
+        for(int i=0;i<60;i++){
+           for(int j=0,j<60;int j;j++){
+               for(int k=0;k<24,k++){
+               System.out.println(""+k+":"+j+":"+1);
+           }
+        }
+    }
+
